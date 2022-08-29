7ff10a9bc8403025d6a1a6f2d01c270515373420
evolF35 <maccyair@gmail.com>
2022-08-29 16:32:40 -0700

    Learning about the DOM

---
+++ new.js
@@ -0,0 +1,9 @@
+
+const conatngo = document.querySelector('#conatiner');
+
+const content = document.createElement('div');
+
+content.classList.add('content');
+content.textContent = 'This is the glorrrius text-content!';
+
+conatngo.appendChild(content);
