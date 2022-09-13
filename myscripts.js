000fbd02277d4a0ef1a5661fc5d2936832302174
evolF35 <maccyair@gmail.com>
2022-09-13 14:11:58 -0700

    completed exercise prototypes

--- myscripts.js
+++ myscripts.js
@@ -8,5 +8,31 @@ function Book(title,author,pages,read){
     this.report = function(){
         return(title+author+pages+read);
     }
-
 }
+
+
+
+
+let head = {
+    glasses: 1
+  };
+  
+  let table = {
+    __proto__: head,
+    pen: 3
+  };
+  
+  let bed = {
+    __proto__: table,
+    sheet: 1,
+    pillow: 2
+  };
+  
+  let pockets = {
+    __proto__: bed,
+    money: 2000
+  };
+
+
+
+  
